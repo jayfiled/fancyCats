eae9b180f7062423e13b157d74cdc0f21c68e174
Jayfiled <jayfiled@gmail.com>
2018-12-06 17:36:47 +0000

    Updates

---
+++ precache-manifest.ad97511c93e047b75ec99e382d7e02a8.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "12a3db8ba7f5423327bb12aed3c41977",
+    "url": "/fancyCats/static/media/SEGA.12a3db8b.woff"
+  },
+  {
+    "revision": "b29145bf9bfb68264793",
+    "url": "/fancyCats/static/js/runtime~main.b29145bf.js"
+  },
+  {
+    "revision": "ed3c0c48451f2892fcdf",
+    "url": "/fancyCats/static/js/main.ed3c0c48.chunk.js"
+  },
+  {
+    "revision": "2a8579eabfd641816414",
+    "url": "/fancyCats/static/js/1.2a8579ea.chunk.js"
+  },
+  {
+    "revision": "ed3c0c48451f2892fcdf",
+    "url": "/fancyCats/static/css/main.b3eca6b4.chunk.css"
+  },
+  {
+    "revision": "2a8579eabfd641816414",
+    "url": "/fancyCats/static/css/1.a3c57544.chunk.css"
+  },
+  {
+    "revision": "578aeb5af75050039ba9c1e65f776a71",
+    "url": "/fancyCats/index.html"
+  }
+];
